3b0e5be8c976f53c9b3fa9193eb1b6231d19e281
Sarah <kunhong.yu@sjsu.edu>
2024-06-26 23:39:36 -0700

    Enhance sidebar appearance with new styles --add logo and app name "CropSense" --add padding to the front of the icons --change to bigger font --add background color and bold the font for the current tab (dashboard in this case)

--- frontend/src/Components/Sidebar.js
+++ frontend/src/Components/Sidebar.js
@@ -1,34 +1,38 @@
 // src/components/Sidebar.js
 import { HomeIcon, UserCircleIcon, Squares2X2Icon, SquaresPlusIcon, ChartPieIcon, MapIcon, CursorArrowRippleIcon } from '@heroicons/react/24/outline';
+import logo from "./styles/Crop.png";
 
 const Sidebar = () => {
   return (
-    <div className="h-screen w-64 bg-gray-800 text-white">
-      <div className="p-4">
-        <h1 className="text-2xl font-bold">Dashboard</h1>
+    <div className="h-screen w-64 bg-white text-black border border-gray-300">
+      <div className="p-4 flex items-center justify-center">
+        <img src={logo} alt="Logo" style={{height: '60px', width: '60px', marginRight: '10px' }} />
+      </div>
+      <div className="p-4 flex items-center justify-center">
+        <h1 className="px-10 text-2xl font-bold">CropSense</h1>
       </div>
       <nav className="mt-5">
-        <a href="#" className="block py-2.5 px-4 rounded transition duration-200 hover:bg-gray-700 hover:text-white flex items-center">
+        <a href="#" className="block text-lg text-indigo-700 font-bold py-4 px-10 rounded transition duration-200 bg-indigo-100 hover:bg-gray-700 hover:text-white flex items-center">
           <HomeIcon className="h-5 w-5 mr-2" />
           Dashboard
         </a>
-        <a href="#" className="block py-2.5 px-4 rounded transition duration-200 hover:bg-gray-700 hover:text-white flex items-center">
+        <a href="#" className="block text-lg py-4 px-10 rounded transition duration-200 hover:bg-gray-700 hover:text-white flex items-center">
           <MapIcon className="h-5 w-5 mr-2" />
           Farm
         </a>
-        <a href="#" className="block py-2.5 px-4 rounded transition duration-200 hover:bg-gray-700 hover:text-white flex items-center">
+        <a href="#" className="block text-lg py-4 px-10 rounded transition duration-200 hover:bg-gray-700 hover:text-white flex items-center">
           <Squares2X2Icon className="h-5 w-5 mr-2" />
           Land
         </a>
-        <a href="#" className="block py-2.5 px-4 rounded transition duration-200 hover:bg-gray-700 hover:text-white flex items-center">
+        <a href="#" className="block text-lg py-4 px-10 rounded transition duration-200 hover:bg-gray-700 hover:text-white flex items-center">
           <SquaresPlusIcon className="h-5 w-5 mr-2" />
           Crop
         </a>
-        <a href="#" className="block py-2.5 px-4 rounded transition duration-200 hover:bg-gray-700 hover:text-white flex items-center">
+        <a href="#" className="block text-lg py-4 px-10 rounded transition duration-200 hover:bg-gray-700 hover:text-white flex items-center">
           <CursorArrowRippleIcon className="h-5 w-5 mr-2" />
           Missions
         </a>
-        <a href="#" className="block py-2.5 px-4 rounded transition duration-200 hover:bg-gray-700 hover:text-white flex items-center">
+        <a href="#" className="block text-lg py-4 px-10 rounded transition duration-200 hover:bg-gray-700 hover:text-white flex items-center">
           <ChartPieIcon className="h-5 w-5 mr-2" />
           Metrics
         </a>
