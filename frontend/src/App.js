d560feb13667fbbe5c7671c05cb46e1000aa68db
Sarah <kunhong.yu@sjsu.edu>
2024-06-27 00:07:50 -0700

    Create Mission Component with sidebar & add link to sidebar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,6 +10,14 @@ import Homepage from './Components/Homepage';
 import UserLogin from './Components/UserLogin';
 import UserSignup from './Components/UserSignUp';
 import UserDashboard from './Components/UserDashboard';
+import Farm from './Components/Farm';
+import Land from './Components/Land';
+import Crop from './Components/Crop';
+import Metrics from './Components/Metrics';
+import Mission from './Components/Mission';
+
+
+
 
 // function App() {
 //   return (
@@ -40,6 +48,11 @@ function App(){
       <Route exact path="/" component={Homepage}/>
       <Route exact path="/userLogin" component={UserLogin}/>
       <Route exact path="/userSignUp" component={UserSignup}/>
+        <Route exact path="/Farm" component={Farm}/>
+        <Route exact path="/Land" component={Land}/>
+        <Route exact path="/Crop" component={Crop}/>
+        <Route exact path="/Metrics" component={Metrics}/>
+          <Route exact path="/Mission" component={Mission}/>
       <Route exact path="/userDashboard" component={UserDashboard}/>
       </Switch>
       </BrowserRouter>
--- frontend/src/Components/Sidebar.js
+++ frontend/src/Components/Sidebar.js
@@ -12,27 +12,27 @@ const Sidebar = () => {
         <h1 className="px-10 text-2xl font-bold">CropSense</h1>
       </div>
       <nav className="mt-5">
-        <a href="#" className="block text-lg text-indigo-700 font-bold py-4 px-10 rounded transition duration-200 bg-indigo-100 hover:bg-gray-700 hover:text-white flex items-center">
+        <a href="/UserDashboard" className="block text-lg text-indigo-700 font-bold py-4 px-10 rounded transition duration-200 bg-indigo-100 hover:bg-gray-700 hover:text-white flex items-center">
           <HomeIcon className="h-5 w-5 mr-2" />
           Dashboard
         </a>
-        <a href="#" className="block text-lg py-4 px-10 rounded transition duration-200 hover:bg-gray-700 hover:text-white flex items-center">
+        <a href="/Farm" className="block text-lg py-4 px-10 rounded transition duration-200 hover:bg-gray-700 hover:text-white flex items-center">
           <MapIcon className="h-5 w-5 mr-2" />
           Farm
         </a>
-        <a href="#" className="block text-lg py-4 px-10 rounded transition duration-200 hover:bg-gray-700 hover:text-white flex items-center">
+        <a href="/Land" className="block text-lg py-4 px-10 rounded transition duration-200 hover:bg-gray-700 hover:text-white flex items-center">
           <Squares2X2Icon className="h-5 w-5 mr-2" />
           Land
         </a>
-        <a href="#" className="block text-lg py-4 px-10 rounded transition duration-200 hover:bg-gray-700 hover:text-white flex items-center">
+        <a href="/Crop" className="block text-lg py-4 px-10 rounded transition duration-200 hover:bg-gray-700 hover:text-white flex items-center">
           <SquaresPlusIcon className="h-5 w-5 mr-2" />
           Crop
         </a>
-        <a href="#" className="block text-lg py-4 px-10 rounded transition duration-200 hover:bg-gray-700 hover:text-white flex items-center">
+        <a href="/Mission" className="block text-lg py-4 px-10 rounded transition duration-200 hover:bg-gray-700 hover:text-white flex items-center">
           <CursorArrowRippleIcon className="h-5 w-5 mr-2" />
           Missions
         </a>
-        <a href="#" className="block text-lg py-4 px-10 rounded transition duration-200 hover:bg-gray-700 hover:text-white flex items-center">
+        <a href="/Metrics" className="block text-lg py-4 px-10 rounded transition duration-200 hover:bg-gray-700 hover:text-white flex items-center">
           <ChartPieIcon className="h-5 w-5 mr-2" />
           Metrics
         </a>
