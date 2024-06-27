1462b6ff8468b1b068c72d8f7b0c95d4f738e782
Sarah <kunhong.yu@sjsu.edu>
2024-06-27 00:33:16 -0700

    Use the "useLocation" hook from "react-router-dom" to highlight the corresponding button in the sidebar based on the current page,

--- frontend/src/Components/Sidebar.js
+++ frontend/src/Components/Sidebar.js
@@ -1,8 +1,15 @@
 // src/components/Sidebar.js
 import { HomeIcon, UserCircleIcon, Squares2X2Icon, SquaresPlusIcon, ChartPieIcon, MapIcon, CursorArrowRippleIcon } from '@heroicons/react/24/outline';
 import logo from "./styles/Crop.png";
+import { Link, useLocation } from 'react-router-dom';
+
 
 const Sidebar = () => {
+  const location = useLocation();
+
+  // Function to check if the link is active
+  const isActive = (path) => location.pathname === path;
+
   return (
     <div className="h-screen w-64 bg-white text-black border border-gray-300">
       <div className="p-4 flex items-center justify-center">
@@ -11,31 +18,68 @@ const Sidebar = () => {
       <div className="p-4 flex items-center justify-center">
         <h1 className="px-10 text-2xl font-bold">CropSense</h1>
       </div>
+
       <nav className="mt-5">
-        <a href="/UserDashboard" className="block text-lg text-indigo-700 font-bold py-4 px-10 rounded transition duration-200 bg-indigo-100 hover:bg-gray-700 hover:text-white flex items-center">
+       <Link
+          to="/UserDashboard"
+          className={`block text-lg py-4 px-10 rounded transition duration-200 flex items-center ${
+            isActive('/UserDashboard') ? 'text-indigo-700 font-bold bg-indigo-100' : 'hover:bg-gray-700 hover:text-white'
+          }`}
+        >
           <HomeIcon className="h-5 w-5 mr-2" />
           Dashboard
-        </a>
-        <a href="/Farm" className="block text-lg py-4 px-10 rounded transition duration-200 hover:bg-gray-700 hover:text-white flex items-center">
+        </Link>
+
+          <Link
+          to="/Farm"
+          className={`block text-lg py-4 px-10 rounded transition duration-200 flex items-center ${
+            isActive('/Farm') ? 'text-indigo-700 font-bold bg-indigo-100' : 'hover:bg-gray-700 hover:text-white'
+          }`}
+        >
           <MapIcon className="h-5 w-5 mr-2" />
           Farm
-        </a>
-        <a href="/Land" className="block text-lg py-4 px-10 rounded transition duration-200 hover:bg-gray-700 hover:text-white flex items-center">
+        </Link>
+
+          <Link
+          to="/Land"
+          className={`block text-lg py-4 px-10 rounded transition duration-200 flex items-center ${
+            isActive('/Land') ? 'text-indigo-700 font-bold bg-indigo-100' : 'hover:bg-gray-700 hover:text-white'
+          }`}
+        >
           <Squares2X2Icon className="h-5 w-5 mr-2" />
           Land
-        </a>
-        <a href="/Crop" className="block text-lg py-4 px-10 rounded transition duration-200 hover:bg-gray-700 hover:text-white flex items-center">
+        </Link>
+
+          <Link
+          to="/Crop"
+          className={`block text-lg py-4 px-10 rounded transition duration-200 flex items-center ${
+            isActive('/Crop') ? 'text-indigo-700 font-bold bg-indigo-100' : 'hover:bg-gray-700 hover:text-white'
+          }`}
+        >
           <SquaresPlusIcon className="h-5 w-5 mr-2" />
           Crop
-        </a>
-        <a href="/Mission" className="block text-lg py-4 px-10 rounded transition duration-200 hover:bg-gray-700 hover:text-white flex items-center">
+        </Link>
+
+          <Link
+          to="/Mission"
+          className={`block text-lg py-4 px-10 rounded transition duration-200 flex items-center ${
+            isActive('/Mission') ? 'text-indigo-700 font-bold bg-indigo-100' : 'hover:bg-gray-700 hover:text-white'
+          }`}
+        >
           <CursorArrowRippleIcon className="h-5 w-5 mr-2" />
-          Missions
-        </a>
-        <a href="/Metrics" className="block text-lg py-4 px-10 rounded transition duration-200 hover:bg-gray-700 hover:text-white flex items-center">
+          Mission
+        </Link>
+
+          <Link
+          to="/Metrics"
+          className={`block text-lg py-4 px-10 rounded transition duration-200 flex items-center ${
+            isActive('/Metrics') ? 'text-indigo-700 font-bold bg-indigo-100' : 'hover:bg-gray-700 hover:text-white'
+          }`}
+        >
           <ChartPieIcon className="h-5 w-5 mr-2" />
           Metrics
-        </a>
+        </Link>
+
       </nav>
     </div>
   );
