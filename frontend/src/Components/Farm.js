b57ef2b68b135f1ef1cde7a410f09e7d7a249ef9
Sarah <kunhong.yu@sjsu.edu>
2024-07-19 08:41:20 -0700

    Enabled Center Farm function with google maps API

--- frontend/src/Components/Farm.js
+++ frontend/src/Components/Farm.js
@@ -1,12 +1,13 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import Modal from 'react-modal';
-import { MapContainer, TileLayer, Marker, useMapEvents, Polygon } from 'react-leaflet';
+import { MapContainer, TileLayer, Marker, Polygon, useMap, useMapEvents } from 'react-leaflet';
 import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
 import Sidebar from "./Sidebar";
 import { MapPinIcon } from '@heroicons/react/solid';
 import axios from 'axios';
 import { useHistory } from 'react-router-dom';
+import 'leaflet.gridlayer.googlemutant';
 
 import UserDashboard from "./UserDashboard";
 
@@ -21,6 +22,8 @@ function Farm() {
     zip: '',
   });
   const [pins, setPins] = useState([]);
+  const [mapCenter, setMapCenter] = useState([37.003, -121.557]);
+  const mapRef = useRef(null);
 
   useEffect(() => {
     // Example fetch call to simulate data loading
@@ -36,13 +39,10 @@ function Farm() {
   const handleView = (id) => {
     alert(`View Farm with ID: ${id}`);
     history.push(`/farmDetail/${id}`);
-
-    // Implement view logic here
   };
 
   const handleDelete = (id) => {
     alert(`Delete Farm with ID: ${id}`);
-    // Implement delete logic here
   };
 
   const openModal = () => {
@@ -62,10 +62,29 @@ function Farm() {
     });
   };
 
-  const handleCenterFarm = () => {
-    // Center the map based on the address
-    // You can use a geocoding service to get coordinates from the address
-  };
+const handleCenterFarm = async () => {
+  const address = `${form.street}, ${form.city}, ${form.zip}`;
+  const geocodeUrl = `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(address)}&key=AIzaSyCa7zU1MY1HQc_vmy1m3Wq4v13LGZXSvt0`;
+
+  try {
+    const response = await axios.get(geocodeUrl);
+    console.log("Geocode API Response:", response.data); // Log the full response for debugging
+
+    if (response.data.status === "OK" && response.data.results.length > 0) {
+      const { lat, lng } = response.data.results[0].geometry.location;
+      setMapCenter([lat, lng]);
+      if (mapRef.current) {
+        mapRef.current.setView([lat, lng], 16);
+      }
+    } else {
+      console.error("Geocoding failed: No results found.");
+    }
+  } catch (error) {
+    console.error("Error geocoding the address:", error);
+  }
+};
+
+
 
   const addPin = (e) => {
     if (pins.length < 4) {
@@ -74,36 +93,31 @@ function Farm() {
   };
 
   const MapEvents = () => {
+    const map = useMap();
     useMapEvents({
       click: addPin,
     });
+
+    useEffect(() => {
+      mapRef.current = map;
+    }, [map]);
+
     return null;
-  }
+  };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    // Make an API call to save the farm to the database
     const newFarm = {
       id: data.length + 1, // This is just a placeholder.
       name: form.name,
       location: `${form.street}, ${form.city}, ${form.zip}`,
     };
-    // Example of API call:
-    // await fetch('/api/farms', {
-    //   method: 'POST',
-    //   headers: {
-    //     'Content-Type': 'application/json',
-    //   },
-    //   body: JSON.stringify(newFarm),
-    // });
-
-    // Update the local state to include the new farm
     setData([...data, newFarm]);
     closeModal();
   };
 
   // Custom icon using Heroicons MapPinIcon
- const customIconHtml = `
+  const customIconHtml = `
     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="red" class="size-6">
       <path stroke-linecap="round" stroke-linejoin="round" d="M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
       <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1 1 15 0Z" />
@@ -118,58 +132,59 @@ function Farm() {
   });
 
   return (
-    <div className="flex h-screen ">
+    <div className="flex h-screen">
+
       <div className="flex-1 flex justify-center items-start pt-40">
         <div className="w-3/4">
           <div className="relative">
-          {/* Create Farm Button */}
-          <button
-            className="absolute top-1/2 right-0 transform -translate-y-1/2 bg-indigo-500 text-white py-2 px-4 rounded shadow hover:bg-indigo-700"
-            onClick={openModal}
-          >
-            Create Farm
-          </button>
-        </div>
-        {/* Table for showcasing farms */}
-        <div className="mt-16">
-          <h1 className="text-3xl font-bold mb-4">Farms</h1>
-          <table className="min-w-full bg-white">
-            <thead>
-              <tr>
-                <th className="py-2 px-4 border-b">ID</th>
-                <th className="py-2 px-4 border-b">Name</th>
-                <th className="py-2 px-4 border-b">Location</th>
-                <th className="py-2 px-4 border-b">Actions</th>
-              </tr>
-            </thead>
-            <tbody>
-              {data.map((farm) => (
-                <tr key={farm.id}>
-                  <td className="py-4 px-4 border-b text-center">{farm.id}</td>
-                  <td className="py-4 px-4 border-b text-center">{farm.name}</td>
-                  <td className="py-4 px-4 border-b text-center">{farm.location}</td>
-                  <td className="py-4 px-4 border-b text-center">
-                    <button
-                      className="bg-indigo-500 text-white py-1 px-3 rounded mr-8 hover:bg-indigo-700"
-                      onClick={() => handleView(farm.id)}
-                    >
-                      View
-                    </button>
-                    <button
-                      className="bg-indigo-500 text-white py-1 px-3 rounded hover:bg-indigo-700"
-                      onClick={() => handleDelete(farm.id)}
-                    >
-                      Delete
-                    </button>
-                  </td>
+            {/* Create Farm Button */}
+            <button
+              className="absolute top-1/2 right-0 transform -translate-y-1/2 bg-indigo-500 text-white py-2 px-4 rounded shadow hover:bg-indigo-700"
+              onClick={openModal}
+            >
+              Create Farm
+            </button>
+          </div>
+          {/* Table for showcasing farms */}
+          <div className="mt-16">
+            <h1 className="text-3xl font-bold mb-4">Farms</h1>
+            <table className="min-w-full bg-white">
+              <thead>
+                <tr>
+                  <th className="py-2 px-4 border-b">ID</th>
+                  <th className="py-2 px-4 border-b">Name</th>
+                  <th className="py-2 px-4 border-b">Location</th>
+                  <th className="py-2 px-4 border-b">Actions</th>
                 </tr>
-              ))}
-            </tbody>
-          </table>
-        </div>
+              </thead>
+              <tbody>
+                {data.map((farm) => (
+                  <tr key={farm.id}>
+                    <td className="py-4 px-4 border-b text-center">{farm.id}</td>
+                    <td className="py-4 px-4 border-b text-center">{farm.name}</td>
+                    <td className="py-4 px-4 border-b text-center">{farm.location}</td>
+                    <td className="py-4 px-4 border-b text-center">
+                      <button
+                        className="bg-indigo-500 text-white py-1 px-3 rounded mr-8 hover:bg-indigo-700"
+                        onClick={() => handleView(farm.id)}
+                      >
+                        View
+                      </button>
+                      <button
+                        className="bg-indigo-500 text-white py-1 px-3 rounded hover:bg-indigo-700"
+                        onClick={() => handleDelete(farm.id)}
+                      >
+                        Delete
+                      </button>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
         </div>
       </div>
-       <Modal
+      <Modal
         isOpen={isModalOpen}
         onRequestClose={closeModal}
         contentLabel="Create Farm"
@@ -201,25 +216,25 @@ function Farm() {
             </div>
             <div className="flex space-x-4">
               <div className="mb-2 w-3/5 mr-4">
-              <label className="block text-gray-700 text-sm font-bold mb-2">City</label>
-              <input
-                type="text"
-                name="city"
-                value={form.city}
-                onChange={handleChange}
-                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-              />
-            </div>
-            <div className="mb-2 w-2/5">
-              <label className="block text-gray-700 text-sm font-bold mb-2">Zip Code</label>
-              <input
-                type="text"
-                name="zip"
-                value={form.zip}
-                onChange={handleChange}
-                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-              />
-            </div>
+                <label className="block text-gray-700 text-sm font-bold mb-2">City</label>
+                <input
+                  type="text"
+                  name="city"
+                  value={form.city}
+                  onChange={handleChange}
+                  className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                />
+              </div>
+              <div className="mb-2 w-2/5">
+                <label className="block text-gray-700 text-sm font-bold mb-2">Zip Code</label>
+                <input
+                  type="text"
+                  name="zip"
+                  value={form.zip}
+                  onChange={handleChange}
+                  className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                />
+              </div>
             </div>
             <button
               type="button"
@@ -230,9 +245,10 @@ function Farm() {
             </button>
           </form>
           <div className="mt-6">
-            <MapContainer center={[37.003, -121.557]} zoom={13} style={{ height: '400px', width: '100%' }}>
+            <MapContainer center={mapCenter} zoom={16} style={{ height: '400px', width: '100%' }} ref={mapRef}>
               <TileLayer
-                url="https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}"
+                url="http://{s}.google.com/vt/lyrs=s&x={x}&y={y}&z={z}"
+                subdomains={['mt0', 'mt1', 'mt2', 'mt3']}
               />
               <MapEvents />
               {pins.map((pin, index) => (
@@ -244,19 +260,19 @@ function Farm() {
             </MapContainer>
           </div>
           <div className="mt-4 text-right">
-              <button
-                onClick={handleSubmit}
-                className="bg-indigo-400 text-white py-2 px-4 rounded shadow hover:bg-green-700"
-              >
-                Create
-              </button>
-              <button
-                onClick={closeModal}
-                className="bg-gray-400 text-white py-2 px-4 rounded shadow hover:bg-gray-700 ml-2"
-              >
-                Close
-              </button>
-            </div>
+            <button
+              onClick={handleSubmit}
+              className="bg-indigo-400 text-white py-2 px-4 rounded shadow hover:bg-green-700"
+            >
+              Create
+            </button>
+            <button
+              onClick={closeModal}
+              className="bg-gray-400 text-white py-2 px-4 rounded shadow hover:bg-gray-700 ml-2"
+            >
+              Close
+            </button>
+          </div>
         </div>
       </Modal>
     </div>
