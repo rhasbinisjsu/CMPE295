1bd758c92271753456cc9c0aa941419801760433
rhasbinisjsu <98677825+rhasbinisjsu@users.noreply.github.com>
2024-07-21 12:21:11 -0700

    support for species anomalies - metrics_server

---
+++ backend/metrics_server/Controllers/FarmMetricsController.java
@@ -0,0 +1,65 @@
+package com.cropsense.metrics_server.Controllers;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.cropsense.metrics_server.Logger.AppLogger;
+import com.cropsense.metrics_server.Services.FarmMetricsService;
+
+@RestController
+@RequestMapping("/CropSense/MetricsServer/FarmMetricsController")
+public class FarmMetricsController {
+    
+    @Autowired 
+    private FarmMetricsService fmService;
+
+    // Declare and instantiate logger
+    private final AppLogger logger = new AppLogger(getClass().toString());
+
+
+    /**
+     * Get count of farms owned by farmer
+     * @param ownerId
+     * @return
+     * @throws IOException
+     * @throws URISyntaxException
+     * @throws InterruptedException
+     */
+    @GetMapping(
+        path = "/getOwnerFarmCount",
+        produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public ResponseEntity<?> getOwnerFarmCount(
+        @RequestParam long ownerId
+    ) throws IOException, URISyntaxException, InterruptedException {
+
+        logger.logInfoMsg("[ ENTER ] Received request to count serviced farms for owner with ID: " + Long.toString(ownerId));
+
+        ResponseEntity<?> response;
+
+        int farmCount;
+        try {
+            farmCount = fmService.getOwnerFarmCount(ownerId);
+            response = new ResponseEntity<String>(String.valueOf(farmCount), HttpStatus.OK);
+        }
+        catch (Exception e) {
+            logger.logErrorMsg("Caught Exception... " + e.getCause() + "\nReason: " + e.getMessage());
+            response = new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+            throw e; 
+        }
+
+        logger.logInfoMsg("[ EXIT ] Exiting request to count farms for owner with ID: " + Long.toString(ownerId));
+        return response;
+
+    }
+
+}
---
+++ backend/metrics_server/Controllers/SoilMetricsController.java
@@ -0,0 +1,63 @@
+package com.cropsense.metrics_server.Controllers;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.HashMap;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.cropsense.metrics_server.Logger.AppLogger;
+import com.cropsense.metrics_server.Services.SoilMetricsService;
+
+@RestController
+@RequestMapping("/CropSense/MetricsServer/SoilMetricsController")
+public class SoilMetricsController {
+
+    @Autowired
+    private SoilMetricsService smService;
+
+    private final AppLogger logger = new AppLogger(getClass().toString());
+
+    
+    /**
+     * 
+     * @param cropId
+     * @return
+     * @throws URISyntaxException
+     * @throws IOException
+     * @throws InterruptedException
+     */
+    @GetMapping(
+        path = "/getIndividualizedMetricsForCrop",
+        produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public ResponseEntity<?> getIndividualizedMetricsForCrop(
+        @RequestParam long cropId
+    ) throws URISyntaxException, IOException, InterruptedException {
+        
+        logger.logInfoMsg("[ ENTER ] Receieved request to get individualized soil metrics for crop with ID: " + Long.toString(cropId));
+        ResponseEntity<?> response;
+
+        try {
+            HashMap<String,HashMap<String,String>> responseHash = smService.getIndividualSoilMetricsForCrop(cropId);
+            response = new ResponseEntity<HashMap<String,HashMap<String,String>>>(responseHash, HttpStatus.OK);
+        }
+        catch (Exception e) {
+            response = new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+            logger.logErrorMsg("Caught exception... Failed to fetch and generate individualized metrics \n Reason: " + e.getMessage());
+            throw e;
+        }
+
+        logger.logInfoMsg("[ EXIT ] Exiting request to get individualized soil metrics for crop with ID: " + Long.toString(cropId));
+        return response;
+
+    }
+    
+}
