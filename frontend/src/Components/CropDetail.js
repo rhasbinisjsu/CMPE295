337e884bc150a958d95c5362bc5cc52dbaa20139
Sarah <kunhong.yu@sjsu.edu>
2024-07-21 23:54:40 -0700

    added the anomaly metrics section, and used different line graph colors for different sections.

--- frontend/src/Components/CropDetail.js
+++ frontend/src/Components/CropDetail.js
@@ -2,15 +2,15 @@ import React, { useState } from 'react';
 import LineGraph from './LineGraph'; // Assuming you have this component
 import { Tab } from '@headlessui/react';
 import Chart from 'react-apexcharts';
-import { CameraIcon } from '@heroicons/react/24/solid';
+import { CameraIcon, BugAntIcon } from '@heroicons/react/24/solid';
 
 
 const mockSoilMetricsData = [
-  { title: 'Soil Metric 1', data: [10, 20, 30, 40, 50] },
-  { title: 'Soil Metric 2', data: [15, 25, 35, 45, 55] },
-  { title: 'Soil Metric 3', data: [20, 30, 40, 50, 60] },
-  { title: 'Soil Metric 4', data: [25, 35, 45, 55, 65] },
-  { title: 'Soil Metric 5', data: [30, 40, 50, 60, 70] }
+  { title: 'Potassium', data: [20, 20, 5, 20, 10] },
+  { title: 'Nitrogen', data: [30, 25, 30, 35, 25] },
+  { title: 'ph', data: [7.5, 7, 6.9, 6, 6.5] },
+  { title: 'Moisture', data: [25, 35, 45, 55, 65] },
+  { title: 'Metric 5', data: [30, 40, 50, 60, 70] }
 ];
 
 const mockDiseaseData = [
@@ -32,7 +32,7 @@ const CropDetail = () => {
     <div className="container mx-auto p-6 bg-gray-100">
       <h1 className="text-3xl font-bold mb-6">Crop Detail</h1>
       <div className="flex justify-center mt-6">
-        <div className="bg-white p-4 rounded-lg shadow justify-center w-1/2 ">
+        <div className="bg-white p-4 rounded-lg shadow-md justify-center w-1/2 ">
         <h2 className="text-2xl font-semibold mb-4">Soil Metrics</h2>
         <Tab.Group selectedIndex={selectedMetricIndex} onChange={setSelectedMetricIndex}>
           <Tab.List className="flex space-x-2 mb-4">
@@ -41,7 +41,7 @@ const CropDetail = () => {
                 key={index}
                 className={({ selected }) =>
                   `px-4 py-2 text-sm font-medium leading-5 text-white ${
-                    selected ? 'bg-blue-500' : 'bg-blue-300'
+                    selected ? 'bg-yellow-700' : 'bg-yellow-500'
                   } rounded-lg focus:outline-none`
                 }
               >
@@ -51,8 +51,8 @@ const CropDetail = () => {
           </Tab.List>
           <Tab.Panels>
             {mockSoilMetricsData.map((metric, index) => (
-              <Tab.Panel key={index}>
-                <LineGraph title={metric.title} data={metric.data} />
+              <Tab.Panel key={index} className="transition-opacity duration-500 ease-in-out">
+                <LineGraph title={metric.title} data={metric.data} color='#eab308'/>
               </Tab.Panel>
             ))}
           </Tab.Panels>
@@ -63,7 +63,7 @@ const CropDetail = () => {
         <div className="mt-6 grid grid-cols-1 md:grid-cols-2 gap-6">
 
 
-          <div className="bg-white p-4 rounded-lg shadow">
+          <div className="bg-white p-4 rounded-lg shadow-md">
             <h2 className="text-xl font-semibold mb-4">Latest Detected Diseases</h2>
             <table className="min-w-full bg-white">
               <thead>
@@ -86,16 +86,16 @@ const CropDetail = () => {
           </div>
 
         <div className="relative flex flex-col bg-clip-border rounded-xl bg-white text-gray-700 shadow-md">
-        <div className="bg-clip-border mx-4 rounded-xl overflow-hidden bg-gradient-to-tr from-blue-600 to-blue-400 text-white shadow-blue-500/40 shadow-lg absolute -mt-4 grid h-16 w-16 place-items-center">
-          <CameraIcon className="h-6 w-6 text-white" />
+        <div className="bg-clip-border mx-4 rounded-xl overflow-hidden bg-gradient-to-tr from-purple-600 to-purple-400 text-white shadow-blue-500/40 shadow-lg absolute -mt-4 grid h-16 w-16 place-items-center">
+          <BugAntIcon className="h-6 w-6 text-white" />
         </div>
         <div className="p-4 text-right">
-          <p className="block antialiased font-sans text-m leading-normal font-normal text-blue-gray-600">No. of Farms</p>
-          <h4 className="block antialiased tracking-normal font-sans text-2xl font-semibold leading-snug text-blue-gray-900">3</h4>
+          <p className="block antialiased text-xl font-semibold leading-normal font-normal text-blue-gray-600">Latest Disease %</p>
+          <h4 className="block antialiased tracking-normal font-sans text-2xl font-semibold leading-snug text-blue-900">8%</h4>
         </div>
         <div className="border-t border-blue-gray-50 p-4">
           <p className="block antialiased font-sans text-base leading-relaxed font-normal text-blue-gray-600">
-            <strong className="text-green-500">+25%</strong>&nbsp;than last year
+            <strong className="text-green-500">-25%</strong>&nbsp;than last year
           </p>
         </div>
       </div>
@@ -104,12 +104,61 @@ const CropDetail = () => {
       </div>
 
       <div className="flex justify-center mt-6">
-        <div className="mt-6 bg-white p-4 rounded-lg shadow h-fit w-1/2">
+        <div className="mt-6 bg-white p-4 rounded-lg shadow-md h-fit w-1/2">
         <h2 className="text-2xl font-semibold mb-4">Crop Disease Percentage</h2>
-        <LineGraph  data={mockCropPercentageData.series[0].data} categories={mockCropPercentageData.categories} />
+        <LineGraph  data={mockCropPercentageData.series[0].data} categories={mockCropPercentageData.categories} color='#a855f7'/>
+        </div>
+      </div>
+ <div className="flex justify-center mt-6">
+        <div className="mt-6 grid grid-cols-1 md:grid-cols-2 gap-6">
+
+
+          <div className="bg-white p-4 rounded-lg shadow-md">
+            <h2 className="text-xl font-semibold mb-4">Latest Species Anomalies</h2>
+            <table className="min-w-full bg-white">
+              <thead>
+                <tr>
+                  <th className="py-2 px-4 border-b text-left">Species</th>
+                  <th className="py-2 px-8 border-b text-left">Latitude</th>
+                  <th className="py-2 px-4 border-b text-left">Longitude</th>
+                </tr>
+              </thead>
+              <tbody>
+                {mockDiseaseData.map((item, index) => (
+                  <tr key={index}>
+                    <td className="py-2 px-4 border-b">{item.disease}</td>
+                    <td className="py-2 px-8 border-b">{item.location.latitude}</td>
+                    <td className="py-2 px-4 border-b">{item.location.longitude}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+
+        <div className="relative flex flex-col bg-clip-border rounded-xl bg-white text-gray-700 shadow-md ">
+        <div className="bg-clip-border mx-4 rounded-xl overflow-hidden bg-gradient-to-tr from-pink-600 to-pink-400 text-white shadow-blue-500/40 shadow-lg absolute -mt-4 grid h-16 w-16 place-items-center">
+          <CameraIcon className="h-6 w-6 text-white" />
+        </div>
+        <div className="p-4 text-right">
+          <p className="block antialiased text-xl font-semibold leading-normal font-normal text-blue-gray-600">Anomaly Count</p>
+          <h4 className="block antialiased tracking-normal font-sans text-2xl font-semibold leading-snug text-blue-900">8</h4>
+        </div>
+        <div className="border-t border-blue-gray-50 p-4">
+          <p className="block antialiased font-sans text-base leading-relaxed font-normal text-blue-gray-600">
+            <strong className="text-red-500">+5%</strong>&nbsp;than last week
+          </p>
         </div>
       </div>
 
+      </div>
+      </div>
+
+      <div className="flex justify-center mt-6">
+        <div className="mt-6 bg-white p-4 rounded-lg shadow-md h-fit w-1/2">
+        <h2 className="text-2xl font-semibold mb-4">Anomaly Count</h2>
+        <LineGraph  data={mockCropPercentageData.series[0].data} categories={mockCropPercentageData.categories} color='#ec4899' />
+        </div>
+      </div>
 
     </div>
   );
--- frontend/src/Components/LineGraph.js
+++ frontend/src/Components/LineGraph.js
@@ -1,8 +1,8 @@
+// LineGraph.js
 import React from 'react';
 import Chart from 'react-apexcharts';
 
-const LineGraph = ({ title,data }) => {
-
+const LineGraph = ({ title, data, color }) => {
   const chartOptions = {
     chart: {
       type: 'line',
@@ -20,13 +20,12 @@ const LineGraph = ({ title,data }) => {
     xaxis: {
       categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep'],
     },
-    colors: ['#4DFFDF', '#4DA1FF', '#E323FF', '#7517F8', '#FFD422'],
+    colors: [color], // Correct placement of colors
   };
 
   return (
     <div className="h-full py-6 px-6 rounded-xl border border-gray-200 bg-white">
       <h5 className="text-xl text-gray-700">{title}</h5>
-
       <Chart options={chartOptions} series={chartOptions.series} type="line" />
     </div>
   );
