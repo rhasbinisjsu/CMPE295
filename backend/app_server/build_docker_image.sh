f0883373118ce25940b23ea34c5dbb658b2db8f6
rhasbinisjsu <98677825+rhasbinisjsu@users.noreply.github.com>
2024-07-25 02:16:33 -0700

    Updated app-server code

--- backend/app_server/build_docker_image.sh
+++ backend/app_server/build_docker_image.sh
@@ -1,2 +1,4 @@
 ## Build the docker image
 docker build -t cropsense-application-server:0.0.1-SNAPSHOT-LOCAL .
+
+docker buildx build -t rhasbinisjsu/cropsense-application-server:0.0.1-SNAPSHOT . --platform=linux/amd64,linux/arm64 --push
